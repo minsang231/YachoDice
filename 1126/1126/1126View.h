3db79dbc8df4037b0458e94c29d3dc202da88c3d
kjins1121 <128658778+FFlower2231@users.noreply.github.com>
2025-12-02 13:42:37 +0900

    이미지추가및 UI추가

--- 1126/1126/1126View.h
+++ 1126/1126/1126View.h
@@ -4,14 +4,15 @@
 
 #pragma once
 
-struct DiceInfo {
-	float x, y;       // 위치 (float로 변경 - 부드러운 움직임)
-	float vx, vy;     // 속도 (velocity)
-	float angle;      // 회전 각도
+struct DiceInfo
+{
+	float x, y;		  // 위치 (float로 변경 - 부드러운 움직임)
+	float vx, vy;	  // 속도 (velocity)
+	float angle;	  // 회전 각도
 	float angleSpeed; // 회전 속도
-	int value;        // 주사위 눈 (1~6)
-	bool bRolling;    // 지금 구르는 중인가?
-	bool bKeep;       // 주사위 고정 여부
+	int value;		  // 주사위 눈 (1~6)
+	bool bRolling;	  // 지금 구르는 중인가?
+	bool bKeep;		  // 주사위 고정 여부
 };
 
 class CMy1126View : public CFormView
@@ -22,20 +23,23 @@ protected: // serialization에서만 만들어집니다.
 
 public:
 #ifdef AFX_DESIGN_TIME
-	enum{ IDD = IDD_MY1126_FORM };
+	enum
+	{
+		IDD = IDD_MY1126_FORM
+	};
 #endif
 
-// 특성입니다.
+	// 특성입니다.
 public:
-	CMy1126Doc* GetDocument() const;
-	
-// 작업입니다.
+	CMy1126Doc *GetDocument() const;
+
+	// 작업입니다.
 public:
 	CImage m_image[7];
-	DiceInfo m_dices[5];  // 주사위 5개 배열
+	DiceInfo m_dices[5]; // 주사위 5개 배열
 
-	BOOL m_bIsAnimating;  // 애니메이션 진행 중인지 체크
-	int m_nAniFrame;      // 애니메이션 남은 시간
+	BOOL m_bIsAnimating; // 애니메이션 진행 중인지 체크
+	int m_nAniFrame;	 // 애니메이션 남은 시간
 	void DrawGame();
 	int m_nLeftTime;
 	// [추가] 현재 굴린 횟수 (0 ~ 3)
@@ -54,14 +58,14 @@ public:
 	void UpdateScorePreview();
 
 	// ★★★ [추가] 점수 미리보기용 변수 ★★★
-	int m_nPreviewScores[15];      // 각 족보의 미리보기 점수 저장 (-1이면 표시 안 함)
-	BOOL m_bCanScore[15];          // 해당 족보에 점수를 넣을 수 있는지 (0점 초과 여부)
+	int m_nPreviewScores[15]; // 각 족보의 미리보기 점수 저장 (-1이면 표시 안 함)
+	BOOL m_bCanScore[15];	  // 해당 족보에 점수를 넣을 수 있는지 (0점 초과 여부)
 
 	// [추가] 요트(50점)를 이미 확정 지었는지 기억하는 변수
-	BOOL m_bYachtFixed[2];            // -> 배열로 변경
+	BOOL m_bYachtFixed[2]; // -> 배열로 변경
 
 	// [추가] 각 줄(0~14)의 점수가 확정되었는지 체크하는 배열
-	BOOL m_bScoreFixed[2][15];        // -> 2차원 배열로 변경
+	BOOL m_bScoreFixed[2][15]; // -> 2차원 배열로 변경
 
 	// [추가] 게임 종료 확인 및 점수 집계 함수
 	BOOL CheckGameOver();
@@ -73,7 +77,7 @@ public:
 	int m_nGameMode;
 
 	// ★★★ [추가] 배경 지우기 방지 함수 선언
-	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
+	afx_msg BOOL OnEraseBkgnd(CDC *pDC);
 
 	CFont m_fontButton;
 
@@ -81,43 +85,45 @@ public:
 	CImage m_imgBg; // [추가] 전체 배경 이미지
 
 	// ★★★ [추가] 남은 시간 저장 변수 (초 단위)
-// 재정의입니다.
+	// 재정의입니다.
 public:
-	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
-protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
-	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
-	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
-	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
-	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
-	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
+	virtual BOOL PreCreateWindow(CREATESTRUCT &cs);
 
-// 구현입니다.
+protected:
+	virtual void DoDataExchange(CDataExchange *pDX); // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate();					 // 생성 후 처음 호출되었습니다.
+	virtual BOOL OnPreparePrinting(CPrintInfo *pInfo);
+	virtual void OnBeginPrinting(CDC *pDC, CPrintInfo *pInfo);
+	virtual void OnEndPrinting(CDC *pDC, CPrintInfo *pInfo);
+	virtual void OnPrint(CDC *pDC, CPrintInfo *pInfo);
+
+	// 구현입니다.
 public:
 	virtual ~CMy1126View();
 #ifdef _DEBUG
 	virtual void AssertValid() const;
-	virtual void Dump(CDumpContext& dc) const;
+	virtual void Dump(CDumpContext &dc) const;
 #endif
 
 protected:
-
-// 생성된 메시지 맵 함수
+	// 생성된 메시지 맵 함수
 protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	CListCtrl mlist;
-	afx_msg void OnNMCustomdrawList2(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnNMCustomdrawList2(NMHDR *pNMHDR, LRESULT *pResult);
 	afx_msg void OnTimer(UINT_PTR nIDEvent);
-	virtual void OnDraw(CDC* /*pDC*/);
+	virtual void OnDraw(CDC * /*pDC*/);
 	afx_msg void OnBnClickedButton3();
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
-	afx_msg void OnClickList2(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnClickList2(NMHDR *pNMHDR, LRESULT *pResult);
 	afx_msg void OnBnClickedButton4();
+	void SaveResultToDB(int myScore, int oppScore);
 };
 
-#ifndef _DEBUG  // 1126View.cpp의 디버그 버전
-inline CMy1126Doc* CMy1126View::GetDocument() const
-   { return reinterpret_cast<CMy1126Doc*>(m_pDocument); }
+#ifndef _DEBUG // 1126View.cpp의 디버그 버전
+inline CMy1126Doc *CMy1126View::GetDocument() const
+{
+	return reinterpret_cast<CMy1126Doc *>(m_pDocument);
+}
 #endif
-
--- 1126/1126/resource.h
+++ 1126/1126/resource.h
@@ -10,20 +10,28 @@
 #define IDD_DIALOG1                     311
 #define IDB_BITMAP1                     324
 #define IDB_BITMAP2                     347
+#define IDD_INTRO                       353
+#define IDD_DIALOG_RANK                 355
 #define IDC_LIST2                       1004
 #define IDC_BUTTON3                     1005
 #define IDC_BUTTON4                     1006
 #define IDC_GAME_SCREEN                 1007
 #define IDC_BUTTON1                     1008
 #define IDC_BUTTON2                     1009
+#define IDC_GAME_START                  1010
+#define IDC_BTN_HELP                    1011
+#define IDC_LIST1                       1012
+#define IDC_EDIT1                       1013
+#define IDC_LIST3                       1013
+#define IDC_EDIT2                       1014
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        353
+#define _APS_NEXT_RESOURCE_VALUE        357
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
